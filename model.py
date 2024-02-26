7f7b7493715aa89407554f13a33a49c239335a76
Aymen <Aymen.mokrani@etu.univ-amu.fr>
2024-02-26 17:21:47 +0100

    4 routs added for product table

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import datetime as _dt
 import sqlalchemy as _sql
 import database as _database
 
@@ -6,3 +5,10 @@ class order_status(_database.base):
     __tablename__ = "order_status"
     id = _sql.Column(_sql.INTEGER, primary_key=True,index=True)
     status = _sql.Column(_sql.VARCHAR(200))
+
+class product(_database.base):
+    __tablename__ = "product"
+    id = _sql.Column(_sql.INTEGER, primary_key=True,index=True)
+    category_id = _sql.Column(_sql.INTEGER)
+    name = _sql.Column(_sql.VARCHAR)
+    description = _sql.Column(_sql.TEXT)
--- schemas.py
+++ schemas.py
@@ -9,5 +9,20 @@ class Order_status(_BaseOrder_status):
     class Config:
         from_attributes=True
 
+
 class CreateOrder_status(_BaseOrder_status):
+    pass
+
+class _BaseProduct(_pydantic.BaseModel):
+    category_id : int
+    name : str
+    description : str    
+
+class Product(_BaseProduct):
+    id : int
+
+    class Config:
+        from_attributes=True
+
+class CreateProduct(_BaseProduct):
     pass
