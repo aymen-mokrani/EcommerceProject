f920c269b7e7b16b5ea59ac91239a724c1eac450
Aymen <Aymen.mokrani@etu.univ-amu.fr>
2024-02-26 18:46:00 +0100

    4 services  added for address table

--- schemas.py
+++ schemas.py
@@ -26,3 +26,22 @@ class Product(_BaseProduct):
 
 class CreateProduct(_BaseProduct):
     pass
+
+class _BaseAddress(_pydantic.BaseModel):
+    
+    unit_number : str
+    street_number : str
+    address_line : str
+    city : str
+    region : str
+    postal_code : str
+    country_id : int
+
+class Address(_BaseAddress):
+    id : int
+
+    class Config:
+        from_attributes=True
+
+class CreateAddress(_BaseProduct):
+    pass
--- services.py
+++ services.py
@@ -4,7 +4,6 @@ from faker import Faker
 
 import database as _database
 import model as _models
-import sqlalchemy.orm as _orm
 
 import schemas as _schemas
 
@@ -25,11 +24,14 @@ def get_db():
         db.close()
 
 
-
+#PRODUCT
+        
 async def create_product(db: "Session"
 ) -> _schemas.Product:
     
-    product_data = {"category_id": random.randint(1, 3), "name" : fake.word(), "description" : fake.text()}
+    product_data = { "category_id": random.randint(1, 3)
+                    ,"name" : fake.word()
+                    ,"description" : fake.text()}
     product = _schemas.CreateProduct(**product_data)
     product = _models.product(**product.dict())
     db.add(product)
@@ -59,3 +61,50 @@ async def update_product(product: _models.product,
     db.commit()
     db.refresh(product)
     return _schemas.Product.from_orm(product)
+
+#ADDRESS
+
+async def create_address(db: "Session"
+) -> _schemas.Address:
+    
+    address_data = { "unit_number":  fake.building_number() 
+                    ,"street_number": fake.building_number()
+                    ,"address_line": fake.street_address()
+                    ,"city" : fake.city()
+                    ,"region" : fake.state()
+                    ,"postal_code" : fake.postcode()
+                    ,"country_id" : random.randint(1, 192)}
+    address = _schemas.CreateAddress(**address_data)
+    address = _models.address(**address.dict())
+    db.add(address)
+    db.commit()
+    db.refresh(address)
+    return _schemas.Address.from_orm(address)
+
+async def get_addresses(db: "Session") -> List[_schemas.Address]:
+    address = db.query(_models.address).all()
+    return list(map(_schemas.Address.from_orm, address))
+
+async def get_address(address_id: int, db: "Session"):
+    address = db.query(_models.address).filter(_models.address.id == address_id).first()
+    return address
+
+async def delete_address(address: _models.address, db: "Session"):
+    db.delete(address)
+    db.commit()
+
+async def update_address(address: _models.address, 
+                             db: "Session", 
+                             address_data = _schemas.CreateAddress)-> _schemas.Address:
+    
+    address.unit_number = address_data.unit_number
+    address.street_number = address_data.street_number
+    address.address_line = address_data.address_line
+    address.city = address_data.city
+    address.region = address_data.region
+    address.postal_code = address_data.postal_code
+    address.country_id  = address_data.country_id
+
+    db.commit()
+    db.refresh(address)
+    return _schemas.Address.from_orm(address)
